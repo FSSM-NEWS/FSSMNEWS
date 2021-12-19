cd3054042db53e3e492ce2b5462b2ffb50380867
FSSM-NEWS <96381263+FSSM-NEWS@users.noreply.github.com>
2021-12-19 15:19:45 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,34 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m ;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+   
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+ 
+     $('html, body').animate({scrollTop:0}, 'slow');
+}
